f3037db4e497854b359bb891d3752f4c9cfc12ae
Oscar Nilsson <oscar@osuka.dev>
2024-01-26 02:48:09 +0100

    initial commit

---
+++ dev/src/payload.config.ts
@@ -0,0 +1,44 @@
+import { buildConfig } from 'payload/config';
+import path from 'path';
+import Examples from './collections/Examples';
+import { mongooseAdapter } from '@payloadcms/db-mongodb'
+import { webpackBundler } from '@payloadcms/bundler-webpack'
+import { slateEditor } from '@payloadcms/richtext-slate'
+// @ts-ignore
+import { samplePlugin } from '../../src/index';
+import payload from 'payload';
+
+export default buildConfig({
+  admin: {
+    bundler: webpackBundler(),
+    webpack: config => {
+      const newConfig = {
+        ...config,
+        resolve: {
+          ...config.resolve,
+          alias: {
+            ...(config?.resolve?.alias || {}),
+            react: path.join(__dirname, '../node_modules/react'),
+            'react-dom': path.join(__dirname, '../node_modules/react-dom'),
+            payload: path.join(__dirname, '../node_modules/payload'),
+          },
+        },
+      }
+      return newConfig
+    },
+  },
+  editor: slateEditor({}),
+  collections: [
+    Examples,
+  ],
+  typescript: {
+    outputFile: path.resolve(__dirname, 'payload-types.ts'),
+  },
+  graphQL: {
+    schemaOutputFile: path.resolve(__dirname, 'generated-schema.graphql'),
+  },
+  plugins: [samplePlugin({ enabled: true, secret: process.env.PAYLOAD_SECRET, payload: payload })],
+  db: mongooseAdapter({
+    url: process.env.DATABASE_URI,
+  }),
+})
